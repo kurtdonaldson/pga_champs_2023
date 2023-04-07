48a2cb34c2453c603a915a8b93b2424c685094e6
Kurt Donaldson <kurt_donaldson@hotmail.com>
2023-04-08 07:47:39 +1200

    backend logic complete

--- server.js
+++ server.js
@@ -17,58 +17,65 @@ app.set("view engine", "ejs");
 
 const url = "https://www.espn.com/golf/leaderboard";
 
-async function mastersData() {
-  //Fetch HTML of page to scrape
-  const { data } = await axios.get(url);
+async function getMastersData() {
+  try {
+    //Fetch HTML of page to scrape
+    const { data } = await axios.get(url);
 
-  //Load html in cheerio. We parse it the html data
-  const $ = cheerio.load(data);
+    //Load html in cheerio. We parse it the html data
+    const $ = cheerio.load(data);
 
-  //Selecting the rows of the players
-  const elemSelector =
-    "#fittPageContainer > div:nth-child(3) > div > div.PageLayout__Main > section:nth-child(2) > div > div > div > div.Button--group > div.competitors > div > div > div > div.Table__Scroller > table > tbody > tr";
+    //Selecting the rows of the players
+    const elemSelector =
+      "#fittPageContainer > div:nth-child(3) > div > div.PageLayout__Main > section:nth-child(2) > div > div > div > div.Button--group > div.competitors > div > div > div > div.Table__Scroller > table > tbody > tr";
 
-  //Each player object will be stored here
-  const mastersObjectArray = [];
+    //Each player object will be stored here
+    const mastersObjectArray = [];
 
-  //Now using each method to loop through the list of table rows
-  $(elemSelector).each((parentIndex, parentElement) => {
-    const mastersObject = {};
+    //Now using each method to loop through the list of table rows
+    $(elemSelector).each((parentIndex, parentElement) => {
+      const mastersObject = {};
 
-    let name = $(parentElement).children("td.tl.plyr.Table__TD").text();
+      let nameData = $(parentElement).children("td.tl.plyr.Table__TD").text();
 
-    // let round1 = $(parentElement).children("tr > td:nth-child(8)").text();
+      // let round1 = $(parentElement).children("tr > td:nth-child(8)").text();
 
-    // let round2 = $(parentElement).children("tr > td:nth-child(9)").text();
+      // let round2 = $(parentElement).children("tr > td:nth-child(9)").text();
 
-    // let round3 = $(parentElement).children("tr > td:nth-child(10)").text();
-    // let round4 = $(parentElement).children("tr > td:nth-child(11)").text();
-    // let total = $(parentElement).children("tr > td:nth-child(12)").text();
+      // let round3 = $(parentElement).children("tr > td:nth-child(10)").text();
+      // let round4 = $(parentElement).children("tr > td:nth-child(11)").text();
+      // let total = $(parentElement).children("tr > td:nth-child(12)").text();
 
-    let score = $(parentElement).children("tr > td:nth-child(5)").text();
+      let scoreData = $(parentElement).children("tr > td:nth-child(5)").text();
 
-    mastersObject = { name: score };
+      mastersObject.name = nameData;
+      mastersObject.score = scoreData;
 
-    // console.log(name);
-    // console.log(score);
+      // console.log(nameData);
+      // console.log(scoreData);
 
-    mastersObjectArray.push(mastersObject);
-  });
+      mastersObjectArray.push(mastersObject);
+    });
 
-  console.log(mastersObjectArray);
+    console.log(mastersObjectArray);
 
-  return mastersObjectArray;
+    return mastersObjectArray;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// mastersData();
+app.get("/", async (req, res) => {
+  try {
+    const mastersData = await getMastersData();
+    // console.log(mastersData);
 
-// for (i = 0; i < mastersObject.length; i++) {
-//   console.log(mastersObject(i));
-// }
-
-app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index", { mastersData });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
 app.listen(port, () => {
   console.log(`App listening at port ${port}`);
 });
