409846d1a142058c94f3955f561afe7b3b9d1210
Kurt Donaldson <kurt_donaldson@hotmail.com>
2023-04-08 07:59:11 +1200

    starting frontend

--- server.js
+++ server.js
@@ -38,17 +38,21 @@ async function getMastersData() {
 
       let nameData = $(parentElement).children("td.tl.plyr.Table__TD").text();
 
-      // let round1 = $(parentElement).children("tr > td:nth-child(8)").text();
+      let round1 = $(parentElement).children("tr > td:nth-child(8)").text();
 
-      // let round2 = $(parentElement).children("tr > td:nth-child(9)").text();
+      let round2 = $(parentElement).children("tr > td:nth-child(9)").text();
 
-      // let round3 = $(parentElement).children("tr > td:nth-child(10)").text();
-      // let round4 = $(parentElement).children("tr > td:nth-child(11)").text();
+      let round3 = $(parentElement).children("tr > td:nth-child(10)").text();
+      let round4 = $(parentElement).children("tr > td:nth-child(11)").text();
       // let total = $(parentElement).children("tr > td:nth-child(12)").text();
 
       let scoreData = $(parentElement).children("tr > td:nth-child(5)").text();
 
       mastersObject.name = nameData;
+      mastersObject.round1 = round1;
+      mastersObject.round2 = round2;
+      mastersObject.round3 = round3;
+      mastersObject.round4 = round4;
       mastersObject.score = scoreData;
 
       // console.log(nameData);
